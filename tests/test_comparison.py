b86874547f3a7235118816423d83bebc1070ee74
hsorby <h.sorby@auckland.ac.nz>
2021-09-29 15:39:48 +1300

    Only consider SPARC organisation datasets.

--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -47,6 +47,13 @@ class ComparisonTestCase(unittest.TestCase):
         find_total_response = requests.get(urljoin(pennsieve_host, 'datasets'), params=params, headers=headers)
         self.assertEqual(200, find_total_response.status_code)
 
+        test_response = requests.get(urljoin(pennsieve_host, f'organizations/{Config.SPARC_PENNSIEVE_ORGANISATION_ID}/datasets/metrics'), headers=headers)
+        json_data = test_response.json()
+        datasets = json_data['datasets']
+        sparc_dataset_ids = []
+        for dataset in datasets:
+            sparc_dataset_ids.append(dataset['id'])
+
         json_data = find_total_response.json()
         total_count = json_data['totalCount']
         params = {'limit': total_count, 'embargo': False}
@@ -55,15 +62,23 @@ class ComparisonTestCase(unittest.TestCase):
 
         json_data = response.json()
         discover_doi = []
+        name_doi_map = {}
         for dataset in json_data['datasets']:
-            discover_doi.append(dataset['doi'])
+            if dataset['id'] in sparc_dataset_ids:
+                discover_doi.append(dataset['doi'])
+                name_doi_map[dataset['doi']] = {'name': dataset['name'], 'id': dataset['id']}
 
-        self.assertEqual(total_count, len(discover_doi))
+        # self.assertEqual(total_count, len(discover_doi))
 
         not_found_doi = []
         for test_doi in discover_doi:
             if f'doi:{test_doi}' not in scicrunch_doi:
                 not_found_doi.append(test_doi)
+
+        if len(not_found_doi):
+            print('Not found datasets report:')
+        for doi in not_found_doi:
+            print(f"  {name_doi_map[doi]['id']} - {doi} - {name_doi_map[doi]['name']}")
 
         # Can everything in discover be found on SciCrunch?
         self.assertEqual([], not_found_doi)
