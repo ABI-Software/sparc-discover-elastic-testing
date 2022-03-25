f148b8fb5927bb0769d2012ab782b93bc5ff5cce
David Brooks <dave@bcs.co.nz>
2022-03-25 16:05:16 +1300

    Rewrite connectivity tests to use `mapknowledge` package.

--- tests/config.py
+++ tests/config.py
@@ -7,6 +7,5 @@ class Config(object):
     PENNSIEVE_API_TOKEN = os.environ['PENNSIEVE_API_TOKEN']
     SCICRUNCH_API_HOST = os.environ['SCICRUNCH_API_HOST']
     SCICRUNCH_API_KEY = os.environ['SCICRUNCH_API_KEY']
-    SCICRUNCH_APINATOMY_HOST = os.environ.get('SCICRUNCH_APINATOMY_HOST', 'https://scicrunch.org/api/1/sparc-scigraph/dynamic/demos/apinat')
-    SCICRUNCH_SPARC_CYPHER = os.environ.get('SCICRUNCH_SPARC_CYPHER', 'http://sparc-data.scicrunch.io:9000/scigraph/cypher/execute')
+    SCICRUNCH_API = os.environ.get('SCICRUNCH_API', 'https://scicrunch.org/api/1')
     SPARC_PENNSIEVE_ORGANISATION_ID = 367
