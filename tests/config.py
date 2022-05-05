8fc83145e2e31d90126f32bc2d622ca6ff056d6d
alan-wu <alan.wu@auckland.ac.nz>
2022-05-05 16:18:59 +1200

    Add algolia testing.

--- tests/config.py
+++ tests/config.py
@@ -9,3 +9,6 @@ class Config(object):
     SCICRUNCH_API_KEY = os.environ['SCICRUNCH_API_KEY']
     SCICRUNCH_API = os.environ.get('SCICRUNCH_API', 'https://scicrunch.org/api/1')
     SPARC_PENNSIEVE_ORGANISATION_ID = 367
+    ALGOLIA_KEY=os.environ['ALGOLIA_KEY']
+    ALGOLIA_ID=os.environ['ALGOLIA_ID']
+    ALGOLIA_INDEX=os.environ['ALGOLIA_INDEX']
